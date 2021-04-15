6e72c351584b6914a6563dbb37701bb97d44767a
Nublnv <60975551+Nublnv@users.noreply.github.com>
2021-04-15 15:56:42 +0300

    Create Main.java

---
+++ task_2.3/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        String result = "";
+        for (int i = 0; i < 10; i++)
+        {
+            result += 8;
+            System.out.println(result);
+        }
+    }
+}
