3d731100c1f5c90ddfb571f6766e146d0787e1ff
Nublnv <60975551+Nublnv@users.noreply.github.com>
2021-04-15 15:09:15 +0300

    Create Main.java

---
+++ task_2.1/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        String mama = "Мама";
+        String mila = "Мыла";
+        String rama = "Раму";
+
+        System.out.println(mama + mila + rama);
+        System.out.println(mama + rama + mila );
+        System.out.println(mila + rama + mama);
+        System.out.println(mila + mama + rama);
+        System.out.println(rama + mama + mila);
+        System.out.println(rama + mila + mama);
+    }
+}
