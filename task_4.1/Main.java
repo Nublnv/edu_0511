deba8a1634d54e366a6e06984015b2d5d07c5f59
Nublnv <60975551+Nublnv@users.noreply.github.com>
2021-04-15 19:47:07 +0300

    Create Main.java

---
+++ task_4.1/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        cat.setName("Жужик");
+        System.out.println(cat.getName());
+    }
+}
+
+class Cat {
+    private String name = "безымянный кот";
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName()
+    {
+        return this.name;
+    }
+}
