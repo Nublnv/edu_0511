9d54662812e3e5c53e46c9ae124a01bd2de67bdd
Nublnv <60975551+Nublnv@users.noreply.github.com>
2021-04-15 15:00:02 +0300

    Create Main.java

---
+++ task_1.7/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        int a, b, c;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введиие длину бассейна: ");
+        a = scanner.nextInt();
+        System.out.print("Введиие ширину бассейна: ");
+        b = scanner.nextInt();
+        System.out.print("Введиие высоту бассейна: ");
+        c = scanner.nextInt();
+        System.out.println("Объем бассейна: "+ a*b*c);
+    }
+}
